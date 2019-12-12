28148dcefe0c0d1f2c07ac68368246d8054f8d77
Benoit Paquet <benoxoft@gmail.com>
2019-12-11 19:16:44 -0500

    intcode machine day 5 compatible

--- src/intcode.rs
+++ src/intcode.rs
@@ -1,4 +1,15 @@
-type Program = Vec<i64>;
+pub type Program = Vec<i64>;
+pub type InputGenerator = Box<dyn Fn(&Program)-> i64>;
+pub type OutputHandler = Box<dyn Fn(i64)>;
+
+// Helper functions to plug in IO when it's not needed
+pub fn ZeroInputGenerator() -> InputGenerator {
+    Box::new(|program: &Program| 0)
+}
+
+pub fn NullHandler() -> OutputHandler {
+    Box::new(|output: i64| ())
+}
 
 enum Opcodes {
     Add = 1,
@@ -19,6 +30,13 @@ enum ParameterMode {
     Relative = 2
 }
 
+#[derive(Copy, Clone)]
+enum ParamPos {
+    Param1 = 1,
+    Param2 = 2,
+    Param3 = 3
+}
+
 fn check_resize(program: &mut Program, size: usize) {
     if program.len() < size {
         program.resize(size+2, 0);
@@ -26,46 +44,123 @@ fn check_resize(program: &mut Program, size: usize) {
 }
 
 fn run_opcode_add(program: &mut Program, index: usize, relbase: i64) -> usize {
-    let param1 = get_parameter_X(&program, index, 1, relbase);
-    let param2 = get_parameter_X(&program, index, 2, relbase);
-    let param3 = get_address_X(&program, index, 3, relbase);
+    let param1 = get_parameter_1(&program, index, relbase);
+    let param2 = get_parameter_2(&program, index, relbase);
+    let param3 = get_address_3(&program, index, relbase);
     check_resize(program, param3);
-    println!("ADD {} {} {}", param3, param1, param2);
     program[param3] = param1 + param2;
     index + 4
 }
 
 fn run_opcode_mult(program: &mut Program, index: usize, relbase: i64) -> usize {
-    let param1 = get_parameter_X(&program, index, 1, relbase);
-    let param2 = get_parameter_X(&program, index, 2, relbase);
-    let param3 = get_address_X(&program, index, 3, relbase);
+    let param1 = get_parameter_1(&program, index, relbase);
+    let param2 = get_parameter_2(&program, index, relbase);
+    let param3 = get_address_3(&program, index, relbase);
     check_resize(program, param3);
     program[param3] = param1 * param2;
     index + 4
 }
 
-fn run_opcode_input(program: &mut Program, index: usize, relbase: i64, input: i64) -> usize {
-    let param1 = get_parameter_X(&program, index, 1, relbase) as usize;
+fn run_opcode_input(program: &mut Program, index: usize, relbase: i64, input: &InputGenerator) -> usize {
+    let param1 = get_address_1(&program, index, relbase);
     check_resize(program, param1);
-    program[param1] = input;
+    program[param1] = input(program);
     index + 2
 }
 
-fn get_address_X(program: &Program, index: usize, x: usize, relbase: i64) -> usize {
-    let mode = parse_mode(program[index], x);
-    match mode {
-        ParameterMode::Immediate => index + x,
-        ParameterMode::Position => program[index + x] as usize,
-        ParameterMode::Relative => index + x + relbase as usize
+fn run_opcode_output(program: &mut Program, index: usize, relbase: i64, output: &OutputHandler) -> usize {
+    let param1 = get_parameter_1(&program, index, relbase);
+    output(param1);
+    index + 2
+}
+
+fn run_opcode_jump_if_true(program: &mut Program, index: usize, relbase: i64) -> usize {
+    let param1 = get_parameter_1(&program, index, relbase);
+    let param2 = get_parameter_2(&program, index, relbase) as usize;
+    if param1 != 0 {
+        check_resize(program, param2);
+        param2
+    } else {
+        index + 3
     }
 }
 
-fn get_parameter_X(program: &Program, index: usize, x: usize, relbase: i64) -> i64 {
-    let mode = parse_mode(program[index], x);
+fn run_opcode_jump_if_false(program: &mut Program, index: usize, relbase: i64) -> usize {
+    let param1 = get_parameter_1(&program, index, relbase);
+    let param2 = get_parameter_2(&program, index, relbase) as usize;
+    if param1 != 0 {
+        check_resize(program, param2);
+        param2
+    } else {
+        index + 3
+    }
+}
+
+fn run_opcode_less_than(program: &mut Program, index: usize, relbase: i64) -> usize {
+    let param1 = get_parameter_1(&program, index, relbase);
+    let param2 = get_parameter_2(&program, index, relbase);
+    let param3 = get_address_3(&program, index, relbase) as usize;
+    check_resize(program, param3);
+    program[param3] = if param1 < param2 { 1 } else { 0 };
+    index + 4
+}
+
+fn run_opcode_equals(program: &mut Program, index: usize, relbase: i64) -> usize {
+    let param1 = get_parameter_1(&program, index, relbase);
+    let param2 = get_parameter_2(&program, index, relbase);
+    let param3 = get_address_3(&program, index, relbase) as usize;
+    check_resize(program, param3);
+    program[param3] = if param1 == param2 { 1 } else { 0 };
+    index + 4
+}
+
+fn get_address_1(program: &Program, index: usize, relbase: i64) -> usize {
+    get_address_x(program, index, ParamPos::Param1, relbase)
+}
+
+/*
+fn get_address_2(program: &Program, index: usize, relbase: i64) -> usize {
+    get_address_x(program, index, ParamPos::Param2, relbase)
+}
+*/
+
+fn get_address_3(program: &Program, index: usize, relbase: i64) -> usize {
+    get_address_x(program, index, ParamPos::Param3, relbase)
+}
+
+fn get_address_x(program: &Program, index: usize, ppos: ParamPos, relbase: i64) -> usize {
+    let mode = parse_mode(program[index], ppos);
+    let param_index = ppos as usize;
+
     match mode {
-        ParameterMode::Immediate => program[index + x],
-        ParameterMode::Position => program[program[index + x] as usize],
-        ParameterMode::Relative => program[index + x + relbase as usize]
+        ParameterMode::Immediate => index + param_index,
+        ParameterMode::Position => program[index + param_index] as usize,
+        ParameterMode::Relative => index + param_index + relbase as usize
+    }
+}
+
+fn get_parameter_1(program: &Program, index: usize, relbase: i64) -> i64 {
+    get_parameter_x(program, index, ParamPos::Param1, relbase)
+}
+
+fn get_parameter_2(program: &Program, index: usize, relbase: i64) -> i64 {
+    get_parameter_x(program, index, ParamPos::Param2, relbase)
+}
+
+/*
+fn get_parameter_3(program: &Program, index: usize, relbase: i64) -> i64 {
+    get_parameter_x(program, index, ParamPos::Param3, relbase)
+}
+*/
+
+fn get_parameter_x(program: &Program, index: usize, ppos: ParamPos, relbase: i64) -> i64 {
+    let mode = parse_mode(program[index], ppos);
+    let param_index = ppos as usize;
+
+    match mode {
+        ParameterMode::Immediate => program[index + param_index],
+        ParameterMode::Position => program[program[index + param_index] as usize],
+        ParameterMode::Relative => program[index + param_index + relbase as usize]
     }
 }
 
@@ -87,9 +182,15 @@ fn parse_opcode(value: i64) -> Opcodes {
     opcode
 }
 
-fn parse_mode(value: i64, pos: usize) -> ParameterMode {
+fn parse_mode(value: i64, ppos: ParamPos) -> ParameterMode {
     let strop = (value + 100_000).to_string();
-    let mode = match strop.chars().nth(pos).unwrap() {
+    let mode_pos = match ppos {
+        ParamPos::Param1 => 3,
+        ParamPos::Param2 => 2,
+        ParamPos::Param3 => 1
+    };
+
+    let mode = match strop.chars().nth(mode_pos).unwrap() {
         '0' => ParameterMode::Position,
         '1' => ParameterMode::Immediate,
         '2' => ParameterMode::Relative,
@@ -98,22 +199,21 @@ fn parse_mode(value: i64, pos: usize) -> ParameterMode {
     mode
 }
 
-pub fn run_intcode(program: &mut Vec<i64>, input_generator: usize, output_handler: usize) {
+pub fn run_intcode(program: &mut Vec<i64>, input_generator: &InputGenerator, output_handler: &OutputHandler) {
     let mut index = 0;
     let mut relbase = 0i64;
 
     loop {
         let opcode = parse_opcode(program[index]);
-
         index = match opcode {
             Opcodes::Add => run_opcode_add(program, index, relbase),
             Opcodes::Mult => run_opcode_mult(program, index, relbase),
-            Opcodes::Input => 0,
-            Opcodes::Output => 0,
-            Opcodes::PutIfZero => 0,
-            Opcodes::PutIfNotZero => 0,
-            Opcodes::Put1IfP1LessThanP2 => 0,
-            Opcodes::Put1IfP1EqualsP2 => 0,
+            Opcodes::Input => run_opcode_input(program, index, relbase, input_generator),
+            Opcodes::Output => run_opcode_output(program, index, relbase, output_handler),
+            Opcodes::PutIfZero => run_opcode_jump_if_true(program, index, relbase),
+            Opcodes::PutIfNotZero => run_opcode_jump_if_false(program, index, relbase),
+            Opcodes::Put1IfP1LessThanP2 => run_opcode_less_than(program, index, relbase),
+            Opcodes::Put1IfP1EqualsP2 => run_opcode_equals(program, index, relbase),
             Opcodes::ChangeRelBase => 0,
             Opcodes::Break => std::usize::MAX
         };
--- src/intcode_test.rs
+++ src/intcode_test.rs
@@ -1,13 +1,13 @@
 
 #[cfg(test)]
 mod tests {
-    use crate::intcode::run_intcode;
+    use crate::intcode::{run_intcode, ZeroInputGenerator, NullHandler, InputGenerator, OutputHandler, Program};
 
     #[test]
     fn test_intcode_basic1() {
         let mut program1 = vec![1, 0, 0, 0, 99];
         let result1 = vec![2, 0, 0, 0, 99];
-        run_intcode(&mut program1, 0, 0);
+        run_intcode(&mut program1, &ZeroInputGenerator(), &NullHandler());
         assert_eq!(result1, program1);
     }
     
@@ -15,7 +15,7 @@ mod tests {
     fn test_intcode_basic2() {
         let mut program2 = vec![2, 3, 0, 3, 99];
         let result2 = vec![2, 3, 0, 6, 99];
-        run_intcode(&mut program2, 0, 0);
+        run_intcode(&mut program2, &ZeroInputGenerator(), &NullHandler());
         assert_eq!(result2, program2);
     }
 
@@ -23,7 +23,7 @@ mod tests {
     fn test_intcode_basic3() {
         let mut program3 = vec![2, 4, 4, 5, 99, 0];
         let result3 = vec![2, 4, 4, 5, 99, 9801];
-        run_intcode(&mut program3, 0, 0);
+        run_intcode(&mut program3, &ZeroInputGenerator(), &NullHandler());
         assert_eq!(result3, program3);
     }
 
@@ -31,8 +31,167 @@ mod tests {
     fn test_intcode_basic4() {
         let mut program4 = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
         let result4 = vec![30, 1, 1, 4, 2, 5, 6, 0, 99];
-        run_intcode(&mut program4, 0, 0);
+        run_intcode(&mut program4, &ZeroInputGenerator(), &NullHandler());
         assert_eq!(result4, program4);
     }
-}
 
+    #[test]
+    fn test_prog_day5() {
+        static TEST_VALUE: i64 = 32534;
+
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| TEST_VALUE)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, TEST_VALUE);
+            })
+        };
+
+        let mut program1 = vec![3,0,4,0,99];
+        run_intcode(&mut program1, &test_ig(), &test_oh());
+
+        let mut program2 = vec![1101,100,-1,4,0];
+        run_intcode(&mut program2, &test_ig(), &NullHandler());    
+    }
+
+    #[test]
+    fn test_day5_part2_1() {
+
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 8)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 1);
+            })
+        };
+        let mut program1 = vec![3,9,8,9,10,9,4,9,99,-1,8];
+        run_intcode(&mut program1, &test_ig(), &test_oh());
+    }
+
+    #[test]
+    fn test_day5_part2_2() {
+
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 18)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 0);
+            })
+        };
+        let mut program1 = vec![3,9,8,9,10,9,4,9,99,-1,8];
+        run_intcode(&mut program1, &test_ig(), &test_oh());
+
+    }
+
+    #[test]
+    fn test_day5_part2_3() {
+
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 5)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 1);
+            })
+        };
+        let mut program2 = vec![3,9,7,9,10,9,4,9,99,-1,8];
+        run_intcode(&mut program2, &test_ig(), &test_oh());
+    }
+
+    #[test]
+    fn test_day5_part2_4() {
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 15)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 0);
+            })
+        };
+        let mut program2 = vec![3,9,7,9,10,9,4,9,99,-1,8];
+        run_intcode(&mut program2, &test_ig(), &test_oh());
+    }
+
+    #[test]
+    fn test_day5_part2_5() {
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 8)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 1);
+            })
+        };
+        let mut program3 = vec![3,3,1108,-1,8,3,4,3,99];
+        run_intcode(&mut program3, &test_ig(), &test_oh());
+    }
+
+    #[test]
+    fn test_day5_part2_6() {
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 18)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 0);
+            })
+        };
+        let mut program3 = vec![3,3,1108,-1,8,3,4,3,99];
+        run_intcode(&mut program3, &test_ig(), &test_oh());
+    }
+    
+    #[test]
+    fn test_day5_part2_7() {
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 5)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 1);
+            })
+        };
+        let mut program4 = vec![3,3,1107,-1,8,3,4,3,99];
+        run_intcode(&mut program4, &test_ig(), &test_oh());
+    }
+
+    #[test]
+    fn test_day5_part2_8() {
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 8)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 0);
+            })
+        };
+        let mut program4 = vec![3,3,1107,-1,8,3,4,3,99];
+        run_intcode(&mut program4, &test_ig(), &test_oh());
+    }
+
+    #[test]
+    fn test_jumps() {
+        let test_ig = || -> InputGenerator {
+            Box::new(|_| 0)
+        };
+        
+        let test_oh = || -> OutputHandler {
+            Box::new(|output: i64| {
+                assert_eq!(output, 1);
+            })
+        };
+        let mut program1 = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
+        run_intcode(&mut program1, &test_ig(), &test_oh());
+    }
+}
