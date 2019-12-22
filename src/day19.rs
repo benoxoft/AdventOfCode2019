377cb3f073128bcd83ba50f1ef33c7f351834fc5
Benoit Paquet <benoxoft@gmail.com>
2019-12-22 14:16:19 -0500

    day 19 part 2

--- src/day19.rs
+++ src/day19.rs
@@ -1,4 +1,6 @@
 use crate::intcode::{run_intcode, InputGenerator, OutputHandler};
+use std::fs::File;
+use std::io::Write as IoWrite;
 
 #[aoc_generator(day19)]
 fn parse_input(input: &str) -> Vec<i64> {    
@@ -7,7 +9,7 @@ fn parse_input(input: &str) -> Vec<i64> {
 }
 
 #[aoc(day19, part1)]
-fn find_solution1(input: &Vec<i64>) -> i64 {
+fn find_solution1(input: &Vec<i64>) -> usize {
 
     let mut program = input.clone();
     static mut posx: i64 = 0;
@@ -34,9 +36,9 @@ fn find_solution1(input: &Vec<i64>) -> i64 {
                 output[posy as usize].push(o);
             })
         };
-        for i in 0..10000 {
+        for i in 0..3000 {
             posy = i;
-            for j in 0..10000 {
+            for j in 0..3000 {
                 posx = j;
                 //println!("checking position {} {}", posx, posy);
                 run_intcode(&mut program.clone(), &ig(), &oh());    
@@ -44,8 +46,6 @@ fn find_solution1(input: &Vec<i64>) -> i64 {
             output.push(Vec::new());
         }
 
-        use std::fs::File;
-        use std::io::Write as IoWrite;
         let mut file = File::create("C:\\Users\\Benoit\\foo.txt").unwrap();
     for line in &output {
             for c in line {
@@ -56,7 +56,29 @@ fn find_solution1(input: &Vec<i64>) -> i64 {
             writeln!(file, "{:?}", line);
         }
         file.flush();
+
+        for y in 0..output.len() {
+            for x in 0..output[y].len() {
+                if theres_a_square(&output, (x, y)) {
+                    return x * 10000 + y;
+                }
+            }
+        }
     }
     count
 }
 
+fn theres_a_square(data: &Vec<Vec<i64>>, point: (usize, usize)) -> bool {
+
+    let init_x = point.0 as usize;
+    let init_y = point.1 as usize;
+
+    for x in init_x..init_x + 100 {
+        for y in init_y..init_y + 100 {
+            if data[y][x] == 0 {
+                return false;
+            }
+        }
+    }
+    true
+}
